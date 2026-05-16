f9adda6b381a38684f7cc4a9ec366b855e12187f
chhabrasc8 <chhabrasc8@gmail.com>
2026-05-16 19:03:08 +0530

    merging some changes

--- src/main/java/utils/WebDriverFactory.java
+++ src/main/java/utils/WebDriverFactory.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -27,15 +28,19 @@ public class WebDriverFactory {
 		//driver = new ChromeDriver(); // commented
 		break;
 		
+
 		case "firefox":
-		WebDriverManager.firefoxdriver().setup();
-		driver = new FirefoxDriver();
-		break;
-		
-		case "edge":
-		WebDriverManager.edgedriver().setup();
-		driver = new EdgeDriver();
-		break;
+
+			WebDriverManager.firefoxdriver().setup();
+
+			FirefoxOptions firefoxOptions =
+			        new FirefoxOptions();
+
+			firefoxOptions.addArguments("-headless");
+
+			driver =new FirefoxDriver(firefoxOptions);
+
+			break;
 		
 		}
 		
